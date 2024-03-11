1561578a90cb92bea840fc989358a3176a4421ef
jacob miller <jacobmiller0@protonmail.ch>
2024-03-12 12:15:40 +1300

    a new attempt

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ pwd=$(pwd)
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$pwd/libs/
 lib_flags="-lX11 -lGL -lGLEW -lutils"
 
-gcc -o main main.c -L./libs/ $lib_flags
+gcc -o main.a main.c -L./libs/ $lib_flags
 ./main
